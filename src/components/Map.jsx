8e165c5611f31a60d903697c6249a2058704d840
Elvis <elvisnipah@gmail.com>
2023-03-22 08:32:54 +0000

    remove unused imports

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import { Icon } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
